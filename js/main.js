b7db5baba6694bca68b23286a6cb83a3917451fa
Rodrigovgra <140911975+Rodrigovgra@users.noreply.github.com>
2024-05-05 16:01:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let productos = [];
 
 // Creo un fetch para traer el array almacenado en el json local
-fetch ("/js/productos.json")
+fetch ("js/productos.json")
     .then((res) => res.json())
     .then((data) => {
         productos = data;
